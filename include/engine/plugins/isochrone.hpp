c2c6d0c848f11432faf8ada0a953e9d794eb7966
Mateusz Loskot <mateusz@loskot.net>
2017-08-08 00:15:24 +0200

    [Isochrone Plugin v2] Catch up with changes in OSRM v5.10
    Update to align with changes in various interfaces:
    BasePlugin, DataFacade, PhantomNode, many-to-many search algorithm
    and others used by the Isochrone plug-in (submitted in #3652)
    
    Note, max_weight extra argument added to many-to-many algorithm
    routines in the original implementation has not been ported
    as it appears to be unused.

--- include/engine/plugins/isochrone.hpp
+++ include/engine/plugins/isochrone.hpp
@@ -1,9 +1,10 @@
 #ifndef ISOCHRONEPLUGIN_HPP
 #define ISOCHRONEPLUGIN_HPP
 
+#include "engine/algorithm.hpp"
 #include "engine/api/isochrone_parameters.hpp"
 #include "engine/plugins/plugin_base.hpp"
-#include "engine/routing_algorithms/many_to_many.hpp"
+#include "engine/routing_algorithms.hpp"
 #include "engine/search_engine_data.hpp"
 
 #include <string>
@@ -21,12 +22,12 @@ namespace plugins
 class IsochronePlugin final : public BasePlugin
 {
   private:
-    mutable SearchEngineData heaps;
+    mutable SearchEngineData<routing_algorithms::ch::Algorithm> heaps;
 
   public:
     explicit IsochronePlugin();
 
-    Status HandleRequest(const std::shared_ptr<const datafacade::BaseDataFacade> facade,
+    Status HandleRequest(const RoutingAlgorithmsInterface &algorithms,
                          const api::IsochroneParameters &parameters,
                          std::string &pbf_buffer) const;
 };
